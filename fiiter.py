b5c3ab10b8e5fe0f472f29db7e25585a844900d2
Gia Thy Le <giathy2843@gmail.com>
2023-05-16 17:24:06 +0700

    add option in main

--- fiiter.py
+++ fiiter.py
@@ -2,12 +2,24 @@ import numpy as np
 import cv2 as cv
 
 def median_filter(img_path):
+    print("Median Filtering")
 
-    # using imread()  
     img = cv.imread(img_path)
-    dst = cv.medianBlur(img,7)
+    dst = cv.medianBlur(img,3)
+
     cv.imshow('image', np.hstack((img, dst)))
     cv.waitKey(0)
     cv.destroyAllWindows()
     cv.waitKey(1)
 
+def mean_filter(img_path):
+    print("Mean Filtering")
+    
+    img = cv.imread(img_path)
+
+    new_img = cv.blur(img,(5,5)) 
+
+    cv.imshow('image', np.hstack((img, new_img)))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    cv.waitKey(1)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
-from fiiter import median_filter
+from fiiter import median_filter, mean_filter
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Applying image processing operation on a image')
     parser.add_argument("--image_path", help = "input path to image" )
-    
+    parser.add_argument("--option", type = int, help = " 1) median filter, 2) mean filter")
     args = parser.parse_args()
 
-    median_filter(args.image_path)
-
+    if args.option == 1:
+        median_filter(args.image_path)
+    elif args.option == 2:
+        mean_filter(args.image_path)
