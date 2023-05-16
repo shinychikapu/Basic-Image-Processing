16fc876f8acdbab24d4a94518862e3b653d78c04
Gia Thy Le <giathy2843@gmail.com>
2023-05-16 16:51:40 +0700

    add mean filter function def

--- meanFilter.py
+++ meanFilter.py
@@ -2,14 +2,12 @@ import numpy as np
 import cv2 as cv
 
 def mean_filter(img_path):
-    # image path 
-    path = r'cat.jpg'
 
     # using imread()  
-    img = cv.imread(path)
+    img = cv.imread(img_path)
     dst = cv.medianBlur(img,7)
-
-    cv.imshow('image', numpy.hstack((img, dst)))
-    cv.waitKey(0);
-    cv.destroyAllWindows();
+    cv.imshow('image', np.hstack((img, dst)))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
     cv.waitKey(1)
+
